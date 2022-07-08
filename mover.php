a3786bef5b5bb9e5b4980edc83723750dee92867
Michael Maroszek <paresy@gmail.com>
2022-07-08 09:43:31 +0200

    add first draft for mover

---
+++ mover.php
@@ -0,0 +1,111 @@
+<?php
+
+if (php_sapi_name() != 'cli') {
+    throw new Exception('This application must be run on the command line.');
+}
+
+require __DIR__ . '/client.php';
+require __DIR__ . '/config.php';
+
+use Google\Service\Drive;
+
+// Get the API client and construct the service object.
+$client = getClient();
+$service = new Drive($client);
+
+// Cache FolderIDs for faster access
+$folderCache = [];
+
+// This will move all files from the source folder of my drive to the target folder of the shared drive
+moveFiles($sourceFolderID, $targetFolderID);
+
+// Test Folder generation in target folder
+//echo getTargetFolder($targetFolderID, ["Test Level 1", "Test Level 2", "Test Level 3"]);
+
+function getTargetFolder($targetFolderID, $levelInformation) {
+    global $service, $folderCache;
+
+    $folderID = null;
+    $currentLevel = &$folderCache;
+    foreach($levelInformation as $name) {
+        if (isset($currentLevel[$name])) {
+            $folderID = $currentLevel[$name]["id"];
+        }
+        else {
+            // Search if folder is already created
+            $optParams = [
+                'pageSize' => 1,
+                'fields' => 'files(id)',
+                'driveId' => $targetFolderID,
+                'q' => "mimeType = 'application/vnd.google-apps.folder' and name = '$name'",
+                'supportsAllDrives' => true,
+                'includeItemsFromAllDrives' => true,
+                'corpora' => 'drive',
+            ];
+            if ($folderID) {
+                $optParams['q'] .= "and '$folderID' in parents";
+            }
+            $results = $service->files->listFiles($optParams);
+
+            // Create a new one
+            if (empty($results->getFiles())) {
+                $file = new \Google_Service_Drive_DriveFile();
+                $file->setName($name);
+                $file->setMimeType('application/vnd.google-apps.folder');
+                $file->setParents([$folderID]);
+
+                $folder = $service->files->create($file, [
+                    'supportsAllDrives' => true
+                ]);
+                $folderID = $folder->getId();
+            }
+            else {
+                $folderID = $results->getFiles()[0]->getId();
+            }
+
+            // Map into cache
+            $currentLevel[$name]["id"] = $folderID;
+            $currentLevel[$name]["children"] = [];
+        }
+        $currentLevel = &$currentLevel[$name]["children"];
+    }
+    return $folderID;
+}
+
+function moveFiles($sourceFolderID, $targetFolderID, $levelInformation = [])
+{
+    global $service;
+    do {
+        $optParams = [
+            'pageSize' => 100,
+            'fields' => 'nextPageToken, files(id, mimeType, name, parents, ownedByMe)',
+            'q' => "'$sourceFolderID' in parents",
+        ];
+        if (isset($token)) {
+            $optParams["pageToken"] = $token;
+        }
+        $results = $service->files->listFiles($optParams);
+        foreach ($results->getFiles() as $file) {
+            if ($file->getMimeType() == 'application/vnd.google-apps.folder') {
+                //inspectFolder($file->getId(), array_merge($levelInformation, [$file->getName()]));
+            } else {
+                if ($file->getOwnedByMe()) {
+                    // Move file into new Shared Drive
+                    printf("%s > %s\n", implode(" > ", $levelInformation), $file->getName());
+
+                    $emptyFile = new Google_Service_Drive_DriveFile();
+                    $service->files->update(
+                        $file->getId(),
+                        $emptyFile,
+                        [
+                            'supportsAllDrives' => true,
+                            'addParents' => getTargetFolder($targetFolderID, $levelInformation),
+                            'removeParents' => $file->getParents()
+                        ]
+                    );
+                }
+            }
+        }
+        $token = $results->getNextPageToken();
+    } while($token);
+}
